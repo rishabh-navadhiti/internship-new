d0dfc84714e78fbae267aa5b7a448a13fd7c1db9
Rishabh <rishabh@navadhiti.com>
2025-05-31 07:54:05 +0530

    feature to delete task from the app

--- task-app/routes/taskRoutes.js
+++ task-app/routes/taskRoutes.js
@@ -1,30 +1,20 @@
 const express = require('express');
+
+const router = express.Router();
+
 const path = require('path');
 
 // fire base admin initialization and service account
 
 const admin = require('firebase-admin');
-const serviceAccount = require('./test-project-f4196-firebase-adminsdk-fbsvc-533eab065e.json');
+const serviceAccount = require('..test-project-f4196-firebase-adminsdk-fbsvc-533eab065e.json');
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
 
 const db = admin.firestore();
 
-const app = express();
-app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
-
-const PORT = 3000;
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-
-
-
-app.post('/tasks', async(req, res) => {
+router.post('/tasks', async(req, res) => {
     try {
         const newTask = req.body;
         const taskRef = await db.collection('tasks').add(newTask);
@@ -34,7 +24,7 @@ app.post('/tasks', async(req, res) => {
     }
 });
 
-app.get('/tasks', async(req, res) => {
+router.get('/tasks', async(req, res) => {
     try {
         const snapshot = await db.collection('tasks').get();
         const tasks = [];
@@ -48,20 +38,15 @@ app.get('/tasks', async(req, res) => {
 });
 
 
-app.delete('/tasks/:id', async (req, res) => {
+router.delete('/tasks/:id', async (req, res) => {
   try {
     const taskId = req.params.id;
+
+    // Attempt to delete the document
     await db.collection('tasks').doc(taskId).delete();
 
-    res.status(200).send({ message: `Task with ID ${taskId} deleted.` });
+    res.status(200).send({ message: `Task with ID ${taskId} deleted successfully.` });
   } catch (err) {
     res.status(500).send({ error: 'Failed to delete task', details: err.message });
   }
 });
-
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
-
-
