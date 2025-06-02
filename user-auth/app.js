8cbdad016886ac7cbed8842f4b9e26e3c95f4788
Rishabh S <rishabh4883@gmail.com>
2025-06-02 07:13:52 +0530

    a simple post requst using firebase api to create new user

--- user-auth/app.js
+++ user-auth/app.js
@@ -2,5 +2,38 @@ const express = require('express');
 const getAccessToken = require('./googleAuth');
 const axios = require('axios');
 
-const DB_BASEURL = 'https://firestore.googleapis.com/v1/projects/ user-cred-a841c /databases/(default)/documents'
+const DB_BASEURL = 'https://firestore.googleapis.com/v1/projects/ user-cred-a841c /databases/(default)/documents/users';
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/register', async (req, res) => {
+
+  const user = req.body;
+
+  try {
+    const token = await getAccessToken();
+    const response = axios.post(`${DB_BASEURL}`,
+      {
+        fields: {
+          'fname': {stringValue: user.firstName},
+          'lname': {stringValue: user.lastName},
+          'email': {stringValue: user.email},
+          'password': {stringValue: user.password}
+        }
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        }
+      }
+    );
+    res.send(response.data);
+  } catch (err) {
+    res.status(500).send({error: err.message});
+  }
+})
+
+
 
