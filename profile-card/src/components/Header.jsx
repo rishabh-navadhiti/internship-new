9cf0146857789003efe8a6e19947277567de5aa5
Rishabh <rishabh@navadhiti.com>
2025-06-03 18:21:43 +0530

    Create header with basic data

---
+++ profile-card/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import Button from '@mui/material/Button';
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Celebrity Info</h1>
+            <p>Get the latest info, controvery and gossip</p>
+            <Button variant='outlined'>Hello All</Button>
+        </header>
+    )
+}
+
+export default Header;
