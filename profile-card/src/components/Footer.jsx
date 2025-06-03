31266f1ddeb110f50a2b150e93968b4bc4073cc3
Rishabh <rishabh@navadhiti.com>
2025-06-03 18:21:55 +0530

    Create footer with basic data

---
+++ profile-card/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+        <footer>
+            <h2>Navadhiti</h2>
+            <p>{new Date().getFullYear()}</p>
+        </footer>
+    )
+}
+
+export default Footer;
