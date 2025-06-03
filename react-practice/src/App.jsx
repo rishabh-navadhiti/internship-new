2c4aa1faa599b01effd90a05395a0dc0a83a2f17
Rishabh <rishabh@navadhiti.com>
2025-06-03 18:24:30 +0530

    changes

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -2,21 +2,39 @@ import './app.css';
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
 import Content from './components/Content.jsx';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 export default function App() {
+  console.log(crypto.randomUUID());
+  
+  const places = ['India', 'USA', 'Canada', 'Australia'];
   const contentStyle = {
-    color: '#2a9d8f', // teal shade for better appearance
+    color: '#2a9d8f', 
     fontSize: '20px',
     textAlign: 'center',
   };
+  const btnStyle = {
+		color: 'white',
+		backgroundColor: 'red'
+	}
 
   const myName = "Rishabh";
   return (
     <div>
       <Header name = {myName} />
-      <Content styles={contentStyle} />
+      <Content styles={contentStyle} places={places} />
+      <Button btnStyle={btnStyle} text="Submit" />
       <Footer />
     </div>
   );
+}
+
+const Button = (props) => {
+	return (
+		<button style={props.btnStyle}>{props.text}</button>
+	)
 }
    
--- react-practice/src/components/Content.jsx
+++ react-practice/src/components/Content.jsx
@@ -1,8 +1,18 @@
-const Content = ({styles}) => {
+const  Places = ({places}) => {
+    return (
+        <ul>
+            {places.map(place => <li key={place}>{place}</li>)}
+        </ul>
+    )
+}
+
+const Content = ({styles, places}) => {
     return (
         <div className="content">
             <p style={styles}>Welcome to my Blog</p>
             <p style={styles}>This is a simple React application.</p>
+            <h3>The places I have visited</h3>
+            <Places places={places} />
         </div>
     )
 }
