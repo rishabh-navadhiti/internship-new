168fd5fc603c0da5734efa95f8973b44cd3895f1
Rishabh <rishabh@navadhiti.com>
2025-06-04 14:14:13 +0530

    create jsx for counter and button display

--- counter-input/src/App.jsx
+++ counter-input/src/App.jsx
@@ -1,6 +1,16 @@
 import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
 
+  return (
+    <div className="container">
+      <div>{counter}</div>
+      <button>+</button>
+      <button>-</button>
+    </div>
+  );
   
 }
+
+export default App;
