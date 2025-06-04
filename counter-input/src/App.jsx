0051d7f3aa1fce3a4be9fb958488ef56f954046c
Rishabh <rishabh@navadhiti.com>
2025-06-04 15:25:25 +0530

    Complete input state tracking

--- counter-input/src/App.jsx
+++ counter-input/src/App.jsx
@@ -3,16 +3,41 @@ import Button from "./components/Button";
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [textInput, setTextInput] = useState('');
+  const [time, setTime] = useState(getTime())
 
-  const handleIncrement = () => setCounter(counter + 1);
+  function getTime() {
+    return new Date().toLocaleString().split(' ')[1]
+  }
 
-  const handleDecrement = () => setCounter(counter - 1);
+  const handleIncrement = () => {
+    setCounter(counter + 1); 
+    setTime(getTime());
+  };
+
+  const handleDecrement = () => {
+    setCounter(counter - 1);
+    setTime(getTime());
+  }
+  const handleChange = (e) => setTextInput(e.target.value);
 
   return (
+
+
+
     <div className="container">
-      <div>{counter}</div>
-      <Button text="+" onClick={handleIncrement} />
-      <Button text="-" onClick={handleDecrement} />
+      <div className="counter-container">
+        <div>{counter}</div>
+        <Button text="+" onClick={handleIncrement} />
+        <Button text="-" onClick={handleDecrement} />
+        <div>Counter last updated at: {time}</div>
+      </div>
+
+      <div className="input-container">
+        <label htmlFor="disp">Enter some text: </label>
+        <input type="text" id="disp" value={textInput} onChange={handleChange}/>  
+        <div>Your input: {textInput}</div>
+      </div>
     </div>
   );
   
