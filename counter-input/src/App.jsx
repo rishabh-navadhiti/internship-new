a7e0fc42e2fc8a2c5a6bd35def8e632719af00d8
Rishabh <rishabh@navadhiti.com>
2025-06-04 14:17:29 +0530

    add handlers to increase and decrease counter by 1 on btn press

--- counter-input/src/App.jsx
+++ counter-input/src/App.jsx
@@ -3,11 +3,15 @@ import { useState } from "react";
 function App() {
   const [counter, setCounter] = useState(0);
 
+  const handleIncrement = () => setCounter(counter + 1);
+
+  const handleDecrement = () => setCounter(counter - 1);
+
   return (
     <div className="container">
       <div>{counter}</div>
-      <button>+</button>
-      <button>-</button>
+      <button onClick={handleIncrement}>+</button>
+      <button onClick={handleDecrement}>-</button>
     </div>
   );
   
