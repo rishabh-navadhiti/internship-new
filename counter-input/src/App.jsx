08872782ab851809a4f423ea10a9d66e9797d4b1
Rishabh <rishabh@navadhiti.com>
2025-06-05 14:48:14 +0530

    Styled with material UI

--- counter-input/src/App.jsx
+++ counter-input/src/App.jsx
@@ -4,8 +4,7 @@ import Counter from './components/Counter';
 import InputText from './components/InputText';
 import './App.css'
 import '@fontsource/inter';
-import Card from '@mui/material/Card';
-import { CardContent } from "@mui/material";
+import { Card, CardContent, Typography, Box, Button } from '@mui/material';
 
 
 function App() {
@@ -13,19 +12,19 @@ function App() {
   const [counter, setCounter] = useState(0);
   const [time, setTime] = useState(getTime())
 
-      function getTime() {
-        return new Date().toLocaleString().split(' ')[1]
-    }
+  function getTime() {
+    return new Date().toLocaleString().split(' ')[1]
+  }
 
-    const handleIncrement = () => {
-        setCounter(counter + 1); 
-        setTime(getTime());
-    };
+  const handleIncrement = () => {
+      setCounter(counter + 1); 
+      setTime(getTime());
+  };
 
-    const handleDecrement = () => {
-        setCounter(counter - 1);
-        setTime(getTime());
-    }
+  const handleDecrement = () => {
+      setCounter(counter - 1);
+      setTime(getTime());
+  }
 
   const handleChange = (e) => setTextInput(e.target.value);
 
@@ -35,15 +34,36 @@ function App() {
   }
   return (
 
-    <Card sx={{maxWidth: '400px'}}>
-    <CardContent>
-      <div className="container">
-            <Counter counter={counter} time={time} handleIncrement={handleIncrement} handleDecrement={handleDecrement}/>
-          <InputText textInput={textInput} handleChange={handleChange}/>
-        <button onClick={handleReset}>Reset</button>
-      </div>
-    </CardContent>
-    </Card>
+    <Box sx={{ minHeight: '100vh', bgcolor: '#1b2530', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <Card sx={{ maxWidth: 400, width: '100%', p: 2 }}>
+        <CardContent>
+          <Typography variant="h4" sx={{ textAlign: 'center'}} gutterBottom>Simple Counter App</Typography>
+
+          <Counter
+            counter={counter}
+            time={time}
+            handleIncrement={handleIncrement}
+            handleDecrement={handleDecrement}
+          />
+
+          <InputText textInput={textInput} handleChange={handleChange} />
+
+          <Box sx={{ textAlign: 'center', mt: 2 }}>
+            <Button variant="contained" onClick={handleReset} color="primary">Reset</Button>
+          </Box>
+        </CardContent>
+      </Card>
+    </Box>
+
+  //   <Card sx={{maxWidth: '400px'}}>
+  //   <CardContent>
+  //     <div className="container">
+  //           <Counter counter={counter} time={time} handleIncrement={handleIncrement} handleDecrement={handleDecrement}/>
+  //         <InputText textInput={textInput} handleChange={handleChange}/>
+  //       <button onClick={handleReset}>Reset</button>
+  //     </div>
+  //   </CardContent>
+  //   </Card>
   );
   
 }
