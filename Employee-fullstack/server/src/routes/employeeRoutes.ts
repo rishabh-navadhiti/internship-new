dd59e14b969c6ee5634296c9d02ef30c24418853
Rishabh S <rishabh4883@gmail.com>
2025-06-14 12:56:22 +0530

    add express route for post req, save to mongo

---
+++ Employee-fullstack/server/src/routes/employeeRoutes.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import Employee from '../models/Employee';
+
+const router = Router();
+
+router.post('/', async (req, res) => {
+  try {
+    const employee = new Employee(req.body);
+    await employee.save();
+    res.status(201).json(employee);
+  } catch (err: any) {
+    res.status(500).json({error: err.message})
+  }
+});
+
+export default router;
