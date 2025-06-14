0c4067514dbd142cfca9bceab5e8dc171837a32b
Rishabh S <rishabh4883@gmail.com>
2025-06-14 13:34:04 +0530

    add route to get individual employee details

--- Employee-fullstack/server/src/routes/employeeRoutes.ts
+++ Employee-fullstack/server/src/routes/employeeRoutes.ts
@@ -22,4 +22,15 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/:id', async (req, res) => {
+  try {
+    const employee = await Employee.findById(req.params.id)
+    if (!employee) res.status(404).json({error: 'Not found'});
+    res.status(200).json(employee);
+  } catch (err: any) {
+    res.status(500).json({error: 'Failed to get individual employee from DB', message: err.message});
+  }
+});
+
+
 export default router;
