8c63e551dc51ee182f4043850d9a65e43cb386ae
Rishabh S <rishabh4883@gmail.com>
2025-06-15 14:07:31 +0530

    install and setup chakra ui

--- Employee-fullstack/client/src/main.tsx
+++ Employee-fullstack/client/src/main.tsx
@@ -4,12 +4,15 @@ import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 const queryClient = new QueryClient();
+import { Provider } from './components/ui/provider.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
     <BrowserRouter>
       <StrictMode>
-        <App />
+        <Provider>
+          <App />
+        </Provider>
       </StrictMode>
     </BrowserRouter>
   </QueryClientProvider>
