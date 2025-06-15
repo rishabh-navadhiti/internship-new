b0a5b072d526b7e3817027d1c5978c2b551a56d2
Rishabh S <rishabh4883@gmail.com>
2025-06-15 13:06:05 +0530

    define RHF components, and react query mutation to post data to server

--- Employee-fullstack/client/src/pages/EmployeeForm.tsx
+++ Employee-fullstack/client/src/pages/EmployeeForm.tsx
@@ -1,7 +1,28 @@
+import { useForm } from 'react-hook-form'
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import axios from 'axios';
 
 const EmployeeForm = () => {
+    const { register, handleSubmit, reset, formState: {errors}}  = useForm();
+    const queryClient = useQueryClient();
+    const mutation = useMutation({
+        mutationFn: (newData) => axios.post('http://localhost:3000/employees', newData).then(res => res.data),
+        onSuccess: () => {
+            reset();
+            queryClient.invalidateQueries({queryKey: ['employees']});            
+        }
+    });
+
+    const onSubmit = (data) => {
+        mutation.mutate(data);
+    }
+
     return (
-        <h1>Test</h1>
+        <div>
+            <h1>Test</h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+            </form>
+        </div>
     )
 }
 
