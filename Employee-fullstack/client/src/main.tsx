af9cf1d747ab36ac81628b8cd980c7a6178d0834
Rishabh S <rishabh4883@gmail.com>
2025-06-15 09:14:20 +0530

    get data from backend using tanstack react query

--- Employee-fullstack/client/src/main.tsx
+++ Employee-fullstack/client/src/main.tsx
@@ -2,11 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </BrowserRouter>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </BrowserRouter>
+  </QueryClientProvider>
 )
--- Employee-fullstack/client/src/pages/EmployeeTable.tsx
+++ Employee-fullstack/client/src/pages/EmployeeTable.tsx
@@ -1,7 +1,18 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios'
 
 const EmployeeTable = () => {
+    const { isLoading, error, data } = useQuery({
+        queryKey: ['employees'],
+        queryFn: () => axios.get('http://localhost:3000/employees').then(res => res.data)
+    });
+
+    if (isLoading) console.log('Data loading');
+    if (error) console.log('Error loadng data');
+    console.log(data);
+
     return (
-        <h1>Employee Tabe</h1>
+        <h1>Employee Tabe </h1>
     )
 }
 
